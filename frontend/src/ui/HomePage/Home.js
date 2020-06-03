7767f5de7b33de1e3dbab525d8205696b3d3057a
w3h2b <Zpw@37201769>
2020-06-02 17:05:29 -0700

    setup home and login page

---
+++ frontend/src/ui/HomePage/Home.js
@@ -0,0 +1,14 @@
+
+import React from "react";
+
+function Home() {
+    return (
+        <div>
+            <span>Home</span>
+        </div>
+    );
+}
+
+
+
+export default Home;
---
+++ frontend/src/ui/LoginPage/Login.js
@@ -0,0 +1,14 @@
+
+import React from "react";
+
+function Login() {
+    return (
+        <div>
+            <span>Login</span>
+        </div>
+    );
+}
+
+
+
+export default Login;
