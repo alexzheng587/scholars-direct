65b1b8d964bb2a8627c96f72af0647fbffd76bc8
Colin Zhang <l8v0b@ugrad.cs.ubc.ca>
2020-06-06 01:00:57 -0700

    a

--- scholars-direct-webapp/src/ui/components/App.js
+++ scholars-direct-webapp/src/ui/components/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
-//import { Router, Switch, Route } from 'react-router-dom';
+import { Router, Switch, Route } from 'react-router-dom';
 import {history} from '../../helpers/history';
 import Login from './LoginPage/Login';
 import Home from './HomePage/Home';
 import AboutUs from './HomePage/AboutUs';
 import Header from './Header';
-//import Divider from '@material-ui/core/Divider';
-//import BottomNavigation from '@material-ui/core/BottomNavigation';
-//import Typography from "@material-ui/core/Typography";
+import Divider from '@material-ui/core/Divider';
+import BottomNavigation from '@material-ui/core/BottomNavigation';
+import Typography from "@material-ui/core/Typography";
 import '../styles/App.css';
 import LoginForm from "./LoginPage/LoginForm";
 
@@ -15,21 +15,21 @@ class App extends React.Component {
   render() {
     return (
         <div>
-            <LoginForm/>
-          {/*<Router history={history}>*/}
-          {/*  <Header />*/}
-          {/*  <Switch>*/}
-          {/*    <Route component={Login} path="/login" />*/}
-          {/*    <Route component={AboutUs} path="/aboutUs" />*/}
-          {/*    <Route component={Home} path="/" />*/}
-          {/*  </Switch>*/}
-          {/*</Router>*/}
-          {/*<Divider />*/}
-          {/*<BottomNavigation value="footer" showLabels >*/}
-          {/*  <Typography variant="caption" align={"center"}>*/}
-          {/*    © Copyright 2020*/}
-          {/*  </Typography>*/}
-          {/*</BottomNavigation>*/}
+            {/*<LoginForm/>*/}
+          <Router history={history}>
+            <Header />
+            <Switch>
+              <Route component={Login} path="/login" />
+              <Route component={AboutUs} path="/aboutUs" />
+              <Route component={Home} path="/" />
+            </Switch>
+          </Router>
+          <Divider />
+          <BottomNavigation value="footer" showLabels >
+            <Typography variant="caption" align={"center"}>
+              © Copyright 2020
+            </Typography>
+          </BottomNavigation>
         </div>
     );
   }
