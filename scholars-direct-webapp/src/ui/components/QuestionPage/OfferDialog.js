f01cad12a6c4b299e4a499b1717ac7d78b646e67
Colin Zhang <l8v0b@ugrad.cs.ubc.ca>
2020-07-08 17:40:29 -0700

    offerHelp refactor

--- scholars-direct-webapp/src/ui/components/QuestionPage/OfferDialog.js
+++ scholars-direct-webapp/src/ui/components/QuestionPage/OfferDialog.js
@@ -1,52 +1,102 @@
+import React from "react";
+import {connect} from "react-redux";
+
+import { offerHelp } from "../../../actions";
+import {Button} from 'semantic-ui-react';
 import DialogActions from "@material-ui/core/DialogActions";
-import Button from "@material-ui/core/Button";
 import DialogContentText from "@material-ui/core/DialogContentText";
 import Dialog from "@material-ui/core/Dialog";
 import TextField from "@material-ui/core/TextField";
 import DialogContent from "@material-ui/core/DialogContent";
-import React from "react";
 import DialogTitle from "@material-ui/core/DialogTitle";
 
-export default function OfferDialog(props) {
-    const { onClose, selectedValue, open } = props;
 
-    const handleClose = () => {
-        onClose(selectedValue);
-    };
+class OfferDialog extends React.Component {
+    constructor() {
+        super();
+        this.state = {open: false, time:"", message:"",pid:"",posterID:"",tutorID:""};
+        this.handleClickOpen = this.handleClickOpen.bind(this);
+        this.handleClose = this.handleClose.bind(this);
+        this.setMessage = this.setMessage.bind(this);
+        this.setTime = this.setTime.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    handleClickOpen() {
+        this.setState(e => ({
+            open: true
+        }));
+    }
 
-    return (
-        <Dialog onClose={this.handleClose} aria-labelledby="simple-dialog-title" open={this.state.open}>
-            <DialogTitle id="simple-dialog-title">Offer To Help</DialogTitle>
-            <DialogContent>
-                <DialogContentText>
-                    Post your solution, or schedule a video meeting with the poster.
-                </DialogContentText>
-                <TextField
-                    id="outlined-multiline-flexible"
-                    label="Response"
-                    placeholder="Type your solution here"
-                    multiline
-                    rows={4}
-                    variant="outlined"
-                    fullWidth
-                />
+    handleClose() {
+        this.setState(e => ({
+            open: false
+        }));
+    }
+    setTime(e) {
+        this.setState({
+            time: e.target.value
+        });
+    }
+    setMessage(e) {
+        this.setState({
+            message: e.target.value
+        });
+    }
+    handleSubmit(e) {
+        this.handleClose();
+        // todo get tutorID and posterID
+        this.setState({
+            tutorID: "",
+            posterID: ""
+        });
+        this.props.offerHelp(this.state);
+    }
 
-                <TextField
-                    id="datetime-local"
-                    label="Book a meeting"
-                    type="datetime-local"
-                    defaultValue="2020-05-24T10:30"
-                    margin = 'dense'
-                    InputLabelProps={{
-                        shrink: true,
-                    }}
-                />
-            </DialogContent>
-            <DialogActions>
-                <Button onClick={this.handleClose} color="primary">
-                    Send
-                </Button>
-            </DialogActions>
-        </Dialog>
-    );
+    render() {
+        return (<div>
+                <Button primary onClick={this.handleClickOpen}>Offer to help</Button>
+                <Dialog onClose={this.handleClose} aria-labelledby="simple-dialog-title" open={this.state.open}>
+                    <DialogTitle id="simple-dialog-title">Offer To Help</DialogTitle>
+                    <DialogContent>
+                        <DialogContentText>
+                            Post your solution, or schedule a video meeting with the poster.
+                        </DialogContentText>
+                        <TextField
+                            id="outlined-multiline-flexible"
+                            label="Response"
+                            placeholder="Type your solution here"
+                            multiline
+                            rows={4}
+                            variant="outlined"
+                            fullWidth
+                        />
+
+                        <TextField
+                            id="datetime-local"
+                            label="Book a meeting"
+                            type="datetime-local"
+                            defaultValue="2020-05-24T10:30"
+                            margin = 'dense'
+                            InputLabelProps={{
+                                shrink: true,
+                            }}
+                        />
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={this.handleClose} color="primary">
+                            Send
+                        </Button>
+                    </DialogActions>
+                </Dialog>
+            </div>
+        );
+    }
 }
+
+const mapStateToProps = (state) => {
+    return {
+        messages: state.messages,
+    }
+};
+
+export default connect(mapStateToProps, {offerHelp})(OfferDialog);
--- scholars-direct-webapp/src/ui/components/QuestionPage/Question.js
+++ scholars-direct-webapp/src/ui/components/QuestionPage/Question.js
@@ -1,59 +1,16 @@
 import React from "react";
+import { connect } from 'react-redux';
+import {deleteQuestion} from "../../../actions";
+
 import '../../styles/Form.css';
 import '../../styles/QuestionPage.css';
 import {Item, Image, Button} from 'semantic-ui-react';
-import {Card} from '@material-ui/core';
-import { connect } from 'react-redux';
-import {deleteQuestion, offerHelp} from "../../../actions";
-import DialogTitle from "@material-ui/core/DialogTitle";
-import DialogContent from "@material-ui/core/DialogContent";
-import DialogContentText from "@material-ui/core/DialogContentText";
-import TextField from "@material-ui/core/TextField";
-import DialogActions from "@material-ui/core/DialogActions";
-import Dialog from "@material-ui/core/Dialog";
+
+import OfferDialog from "./OfferDialog";
 
 class Question extends React.Component {
-    constructor() {
-        super();
-        this.state = {open: false, time:"", message:"",pid:"",posterID:"",tutorID:""};
-        this.handleClickOpen = this.handleClickOpen.bind(this);
-        this.handleClose = this.handleClose.bind(this);
-        this.setMessage = this.setMessage.bind(this);
-        this.setTime = this.setTime.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
-    handleClickOpen() {
-        this.setState(e => ({
-            open: true
-        }));
-    }
 
-    handleClose() {
-        this.setState(e => ({
-            open: false
-        }));
-    }
-    setTime(e) {
-        this.setState({
-            time: e.target.value
-        });
-    }
-    setMessage(e) {
-        this.setState({
-            message: e.target.value
-        });
-    }
-    handleSubmit(e) {
-        this.handleClose();
-        // todo get tutorID and posterID
-        this.setState({
-            tutorID: "",
-            posterID: ""
-        });
-            this.props.offerHelp(this.state);
-    }
     render() {
-
         return (<Item className="question-item">
             <Item.Content>
                 <Item.Header as='h4'>{this.props.title}</Item.Header>
@@ -66,41 +23,7 @@ class Question extends React.Component {
                 </Item.Description>
                 <Item.Extra>
                     <div>{this.props.status}</div>
-                    <Button primary onClick={this.handleClickOpen}>Offer to help</Button>
-                    <Dialog onClose={this.handleClose} aria-labelledby="simple-dialog-title" open={this.state.open}>
-                        <DialogTitle id="simple-dialog-title">Offer To Help</DialogTitle>
-                        <DialogContent>
-                            <DialogContentText>
-                                Post your solution, or schedule a video meeting with the poster.
-                            </DialogContentText>
-                            <TextField
-                                id="outlined-multiline-flexible"
-                                label="Response"
-                                placeholder="Type your solution here"
-                                multiline
-                                rows={4}
-                                variant="outlined"
-                                fullWidth
-                                onChange={this.setMessage}
-                            />
-                            <TextField
-                                id="datetime-local"
-                                label="Book a meeting"
-                                type="datetime-local"
-                                defaultValue="2020-05-24T10:30"
-                                margin = 'dense'
-                                InputLabelProps={{
-                                    shrink: true,
-                                }}
-                                onChange={this.setTime}
-                            />
-                        </DialogContent>
-                        <DialogActions>
-                            <Button onClick={this.handleSubmit} color="primary">
-                                Send
-                            </Button>
-                        </DialogActions>
-                    </Dialog>
+                    <OfferDialog/>
                     <Button circular icon='delete' floated='right' onClick={() => this.props.deleteQuestion(this.props.id)}/>
                 </Item.Extra>
             </Item.Content>
@@ -114,4 +37,4 @@ const mapStateToProps = (state) => {
     }
 };
 
-export default connect(mapStateToProps, {deleteQuestion,offerHelp})(Question);
+export default connect(mapStateToProps, {deleteQuestion})(Question);
