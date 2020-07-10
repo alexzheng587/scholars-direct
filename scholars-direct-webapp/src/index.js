87ab56fc5cf087187336f653aecaf80221190284
w3h2b <Zpw@37201769>
2020-07-09 17:21:20 -0700

    a bit more refactor

--- scholars-direct-webapp/src/index.js
+++ scholars-direct-webapp/src/index.js
@@ -1,16 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import 'semantic-ui-css/semantic.min.css';
 import App from './ui/components/App';
-import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
 import {store} from './helpers/store';
-
-
-// setup mock backend
-import { configureFakeBackend } from './helpers/mockBackend';
-configureFakeBackend();
 
 
 ReactDOM.render(
@@ -19,15 +12,3 @@ ReactDOM.render(
     </Provider>,
     document.getElementById('root')
 );
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
