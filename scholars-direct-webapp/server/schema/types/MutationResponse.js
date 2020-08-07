1098d8c30df4ac2a06665fba514c4cfff7bd2cbc
unknown <liu442568275@hotmail.com>
2020-08-07 00:51:18 -0700

    v5

--- scholars-direct-webapp/server/schema/types/MutationResponse.js
+++ scholars-direct-webapp/server/schema/types/MutationResponse.js
@@ -3,12 +3,12 @@ import {
     GraphQLBoolean,
     GraphQLString,
 } from 'graphql';
-
+import User from './User';
 export default new GraphQLObjectType({
     name: 'MutationResponse',
     fields: {
         success: { type: GraphQLBoolean },
-        result: { type: GraphQLObject},
+        result: { type: User},
         message: { type: GraphQLString },
     },
 });
